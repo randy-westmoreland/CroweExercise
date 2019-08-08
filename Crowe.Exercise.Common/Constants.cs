d3f590602cbda966ccdd31f2bb76beb8d4c84cc2
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 09:51:51 -0500

    Added Crowe.Exercise.Common Project and Constants class.

---
+++ Crowe.Exercise.Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Crowe.Exercise.Common
+{
+    /// <summary>
+    /// Crowe Exercise Constants Class.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The appsettings
+        /// </summary>
+        public const string APPSETTINGS = "appsettings.json";
+    }
+}
