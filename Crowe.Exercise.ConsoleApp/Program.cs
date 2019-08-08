d68597539e9e613609494d6a7b4634de4cf31276
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 06:40:28 -0500

    Added appsettings.json and updated Program.cs.
    Added appsettings.json and updated Program.cs to include access to settings file and access to HttpClientFactory.

--- Crowe.Exercise.ConsoleApp/Program.cs
+++ Crowe.Exercise.ConsoleApp/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net.Http;
 
 namespace Crowe.Exercise.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //var collection = new ServiceCollection();
+            //collection.AddHttpClient()
+            //var serviceProvider = collection.BuildServiceProvider();
+
+            //serviceProvider.add
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+
+            var serviceProvider = new ServiceCollection()
+                .AddHttpClient()
+                .BuildServiceProvider();
+
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+
+            var client = httpClientFactory.CreateClient();
+
+            //client.GetAsync()
+
         }
     }
 }
