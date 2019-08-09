1d05054e80dfc84202016a1a37aa539175797544
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 23:38:28 -0500

    Fleshed out IObjectRepository class.

--- Crowe.Exercise.Data/Contracts/IObjectRepository.cs
+++ Crowe.Exercise.Data/Contracts/IObjectRepository.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Crowe.Exercise.Data.Contracts
 {
-    interface IObjectRepository
+    /// <summary>
+    /// IObjectRepository Class.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source.</typeparam>
+    /// <seealso cref="IDisposable" />
+    public interface IObjectRepository<TSource> : IDisposable where TSource : IEntity
     {
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IList<TSource> GetAll();
+
+        /// <summary>
+        /// Gets the entity.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        TSource GetEntity(string key);
+
+        /// <summary>
+        /// Adds the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        int Add(TSource entity);
+
+        /// <summary>
+        /// Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        int Update(TSource entity);
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        int Delete(TSource entity);
     }
 }
