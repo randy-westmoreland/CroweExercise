606339ee9ca38d4ccb5bbaa12590da11c6bef84e
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 19:29:34 -0500

    Updated Crowe.Exercise.Business project.
    Implemented IHelloWorldManager GetMessage method in HelloWorldManager. Added ability to get AppSettingsConfig data and map data to MessageDomainModel.

---
+++ Crowe.Exercise.Business/Contracts/IHelloWorldManager.cs
@@ -0,0 +1,16 @@
+﻿using Crowe.Exercise.Model.Domain;
+
+namespace Crowe.Exercise.Business.Contracts
+{
+    /// <summary>
+    /// IHelloWorldManager Class.
+    /// </summary>
+    public interface IHelloWorldManager
+    {
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        /// <returns>MessageDomainModel</returns>
+        MessageDomainModel GetMessage();
+    }
+}
---
+++ Crowe.Exercise.Business/Managers/HelloWorldManager.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Crowe.Exercise.Business.Contracts;
+using Crowe.Exercise.Model.Domain;
+using Crowe.Exercise.Model.View;
+using Microsoft.Extensions.Options;
+
+namespace Crowe.Exercise.Business.Managers
+{
+    /// <summary>
+    /// HelloWorldManager Class.
+    /// </summary>
+    /// <seealso cref="IHelloWorldManager" />
+    public class HelloWorldManager : IHelloWorldManager
+    {
+        private readonly IOptions<AppSettingsConfig> _appSettings;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HelloWorldManager"/> class.
+        /// </summary>
+        /// <param name="appSettings">The application settings.</param>
+        /// <param name="mapper">The mapper.</param>
+        public HelloWorldManager(IOptions<AppSettingsConfig> appSettings, IMapper mapper)
+        {
+            _appSettings = appSettings;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        /// <returns>
+        /// MessageDomainModel
+        /// </returns>
+        public MessageDomainModel GetMessage()
+        {
+            return _mapper.Map<MessageDomainModel>(_appSettings.Value);
+        }
+    }
+}
