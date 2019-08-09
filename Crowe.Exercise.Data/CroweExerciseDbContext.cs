edbcb800cbf5fce8ef2819e1b8fee923f47a318f
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-09 02:15:22 -0500

    Updated CroweExerciseDbContext and added GlobalSuppressions
    * CroweExerciseContextFactory - This was added as the Data project is not a .net core class library. This allows for DI into .net core and passing in a connection string to the DbContext.
    * CroweExerciseMessagesConfig - Entity config for future enhancements to connecting to a database via Entity Framework.

--- Crowe.Exercise.Data/CroweExerciseDbContext.cs
+++ Crowe.Exercise.Data/CroweExerciseDbContext.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Crowe.Exercise.Data.Config;
+using Crowe.Exercise.Data.Contracts;
+using Crowe.Exercise.Data.Entities;
+using System;
+using System.Data.Entity;
 
 namespace Crowe.Exercise.Data
 {
-    public class CroweExerciseDbContext
+    /// <summary>
+    /// CrowExerciseDbContext Class.
+    /// </summary>
+    /// <seealso cref="DbContext" />
+    /// <seealso cref="IDbContext" />
+    public class CroweExerciseDbContext : DbContext, IDbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CroweExerciseDbContext"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        public CroweExerciseDbContext(string connectionString) : base(connectionString)
+        {
+            // ...
+        }
+
+        /// <summary>
+        /// Gets or sets the messages.
+        /// </summary>
+        /// <value>
+        /// The messages.
+        /// </value>
+        public DbSet<MessageEntity> Messages { get; set; }
+
+        /// <summary>
+        /// Sets the modified.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void SetModified(object entity) => throw new NotImplementedException();
+
+        /// <summary>
+        /// This method is called when the model for a derived context has been initialized, but
+        /// before the model has been locked down and used to initialize the context.  The default
+        /// implementation of this method does nothing, but it can be overridden in a derived class
+        /// such that the model can be further configured before it is locked down.
+        /// </summary>
+        /// <param name="modelBuilder">The builder that defines the model for the context being created.</param>
+        /// <remarks>
+        /// Typically, this method is called only once when the first instance of a derived context
+        /// is created.  The model for that context is then cached and is for all further instances of
+        /// the context in the app domain.  This caching can be disabled by setting the ModelCaching
+        /// property on the given ModelBuidler, but note that this can seriously degrade performance.
+        /// More control over caching is provided through use of the DbModelBuilder and DbContextFactory
+        /// classes directly.
+        /// </remarks>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder?.Configurations.Add(new CroweExerciseMessagesConfig());
+        }
     }
 }
--- Crowe.Exercise.Data/GlobalSuppressions.cs
+++ Crowe.Exercise.Data/GlobalSuppressions.cs
@@ -5,4 +5,10 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>", Scope = "type", Target = "~T:Crowe.Exercise.Data.Contracts.IEntity")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.Repositories.MessageRepository.Add(Crowe.Exercise.Data.Entities.MessageEntity)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.Repositories.MessageRepository.Delete(Crowe.Exercise.Data.Entities.MessageEntity)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.Repositories.MessageRepository.GetAll~System.Linq.IQueryable{Crowe.Exercise.Data.Entities.MessageEntity}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.Repositories.MessageRepository.GetEntity(System.String)~Crowe.Exercise.Data.Entities.MessageEntity")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.Repositories.MessageRepository.Update(Crowe.Exercise.Data.Entities.MessageEntity)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Documentation", "CA1200:Avoid using cref tags with a prefix", Justification = "<Pending>", Scope = "member", Target = "~M:Crowe.Exercise.Data.CroweExerciseContextFactory.Create~Crowe.Exercise.Data.CroweExerciseDbContext")]
 
