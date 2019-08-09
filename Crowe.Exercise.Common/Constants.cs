4c56babea06e59083355ee7fde28b6515f71f87d
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 19:25:40 -0500

    Updated Constatns.cs to include const value for 'Crowe.Exercise.Business' assembly for Scrutor services scan.

--- Crowe.Exercise.Common/Constants.cs
+++ Crowe.Exercise.Common/Constants.cs
@@ -8,11 +8,16 @@
         /// <summary>
         /// The appsettings
         /// </summary>
-        public const string APPSETTINGS                 = "appsettings.json";
+        public const string APPSETTINGS                         = "appsettings.json";
 
         /// <summary>
         /// The crowe exercise settings
         /// </summary>
-        public const string CROWE_EXERCISE_SETTINGS     = "CroweExerciseSettings";
+        public const string CROWE_EXERCISE_SETTINGS             = "CroweExerciseSettings";
+
+        /// <summary>
+        /// The crowe exercise business assembly
+        /// </summary>
+        public const string CROWE_EXERCISE_BUSINESS_ASSEMBLY    = "Crowe.Exercise.Business";
     }
 }
