ebf11cd251245ceaaf3307cf50d7cbc51d2f1269
Randy Westmoreland <randy.westmoreland@gmail.com>
2019-08-08 19:18:21 -0500

    Updated AutoMapperConfig, DependencyConfig and Startup files.
    Made updates to AutoMapperConfig.cs to include mappings between AppSettingsConfig => MessageDomainModel. Also added mappings for MessageDomainModel => MessageApiModel. Updated Startup.cs to add AutoMapper profile and DependencyConfig for DI services. Also added a config setting to setup the Api project so that I could get data from the Api appsettings.json file.

--- Crowe.Exercise.Api/Configurations/AutoMapperConfig.cs
+++ Crowe.Exercise.Api/Configurations/AutoMapperConfig.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Crowe.Exercise.Model.Api;
+using Crowe.Exercise.Model.Domain;
+using Crowe.Exercise.Model.View;
 
 namespace Crowe.Exercise.Api.Configurations
 {
-    public class AutoMapperConfig
+    /// <summary>
+    /// AutoMapperConfig Class.
+    /// </summary>
+    /// <seealso cref="Profile" />
+    public class AutoMapperConfig : Profile
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoMapperConfig"/> class.
+        /// </summary>
+        public AutoMapperConfig()
+        {
+            CreateMap<AppSettingsConfig, MessageDomainModel>()
+                .ForMember(dest => dest.Message,
+                           opts => opts.MapFrom(src => src.MessageToSend))
+                .ForAllOtherMembers(opts => opts.Ignore());
+
+            CreateMap<MessageDomainModel, MessageApiModel>();
+        }
     }
 }
--- Crowe.Exercise.Api/Configurations/DependencyConfig.cs
+++ Crowe.Exercise.Api/Configurations/DependencyConfig.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Crowe.Exercise.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Crowe.Exercise.Api.Configurations
 {
-    public class DependencyConfig
+    /// <summary>
+    /// DependencyConfig Class.
+    /// </summary>
+    public static class DependencyConfig
     {
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="configuration">The configuration.</param>
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var business = Assembly.Load(Constants.CROWE_EXERCISE_BUSINESS_ASSEMBLY);
+
+            services.Scan(x =>
+                x.FromAssemblies(business)
+                .AddClasses(y => y.Where(type => type.Name.EndsWith("Manager", StringComparison.OrdinalIgnoreCase)))
+                .AsImplementedInterfaces()
+                .WithTransientLifetime());
+
+            services.AddSingleton(configuration);
+        }
     }
 }
